310b55ae322326aa48221f6c2fdc28796c5193b5
LeanKhan <eslean20@gmail.com>
2019-05-01 18:32:16 +0100

    Do else

--- bots.js
+++ bots.js
@@ -1,15 +1,25 @@
 const twit = require("twit"),
   config = require("./config.js");
 
+console.log("Bot is alive!");
+
 var Twitter = new twit(config);
+
+var time = new Date();
 
 function tweet() {
   Twitter.post(
     "statuses/update",
-    { status: "Jesus Loves You! ❤️" },
+    {
+      status: `Jesus Loves You! ❤️
+        ${time.toLocaleString()}
+    `
+    },
     (err, data, response) => {
       if (err) {
         console.log("error", err);
+      } else {
+        console.log(data);
       }
     }
   );
