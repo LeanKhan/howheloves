2a7392101c3f7aa1fc9f9ee40ef38d8c0b37422a
LeanKhan <eslean20@gmail.com>
2019-05-01 18:15:48 +0100

    Write tweet code

---
+++ bots.js
@@ -0,0 +1,18 @@
+const twit = require("twit"),
+  config = require("./config.js");
+
+var Twitter = new twit(config);
+
+function tweet() {
+  Twitter.post(
+    "statuses/update",
+    { status: "Jesus Loves You! ❤️" },
+    (err, data, response) => {
+      if (err) {
+        console.log("error", err);
+      }
+    }
+  );
+}
+
+setTimeout(tweet, 14400000);
---
+++ config.js
@@ -0,0 +1,14 @@
+/** Twiiter Configuration keys
+ * - consumer_key
+ * - consumer_secret
+ * - access_token
+ * - access_token_secret
+ *
+ */
+
+module.exports = {
+  consumer_key: "htO1qU2ppw5aoYsH9dWz9z30X",
+  consumer_secret: "h0PjBiy5MUBVca0tRhiYjOlt7O77pCl5r6CJzbuJus7LkN2Q6o",
+  access_token: "1122555032526831616-hQ6YwbJ6PTlp4Niw2YSzFtBwBnDbLd",
+  access_token_secret: "6m9bXEuddmqwMfD1RKuFeZnuOpZ9IQ7skdzEDcjMBNQD5"
+};
