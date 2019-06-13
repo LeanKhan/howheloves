508ab0857c243f47c71b878f82cf3d8df767be40
LeanKhan <eslean20@gmail.com>
2019-06-13 15:00:58 +0100

    Fixed time issue

--- bots.js
+++ bots.js
@@ -1,31 +1,39 @@
 ﻿const twit = require("twit"),
   config = require("./config.js");
 
+// eslint-disable-next-line no-console
 console.log("Bot is alive!");
 
 var Twitter = new twit(config);
 
-var time = new Date();
-
 function tweet() {
+  let time = new Date();
   Twitter.post(
     "statuses/update",
     {
       status: `
-      Jesus Loves You! ❤️
-    ${time.toLocaleString()}
+      Jesus Loves You! ❤️ - ${time.toLocaleString()}
     `
     },
     (err, data, response) => {
       if (err) {
+        // eslint-disable-next-line no-console
         console.log("error", err);
       } else {
-        console.log(data);
+        // eslint-disable-next-line no-console
+        console.log("data => ", data);
+
+        // eslint-disable-next-line no-console
+        console.log("response => ", response);
       }
     }
   );
+  // eslint-disable-next-line no-console
+  console.log("Hey! Tweeting!");
 }
 
-// tweet();
+tweet();
 
-setInterval(tweet, 43200000);
+setInterval(tweet, 86400000);
+
+//  Now tweets every 24 hours
