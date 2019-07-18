f2792727d0fc0a5f03ebd5f6961c227235cfc7a7
LeanKhan <eslean20@gmail.com>
2019-07-18 23:29:28 +0100

    modify: format some html

--- bot.js
+++ bot.js
@@ -5,25 +5,6 @@ var http = require("http"); //create a server object
 
 var port = process.env.PORT || 3000;
 
-http
-  .createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" }); // http header
-
-    var url = req.url;
-
-    if (url === "/") {
-      res.write("<h1>Hi!<h1>"); //write a response
-      res.end(); //end the response
-    } else {
-      res.write("<h1>Jesus Loves You! ❤️<h1>"); //write a response
-      res.end(); //end the response
-    }
-  })
-  .listen(port, function() {
-    // eslint-disable-next-line no-console
-    console.log(`Bot server started at port ${port}`);
-  });
-
 // eslint-disable-next-line no-console
 console.log("Bot is alive!");
 
@@ -35,31 +16,59 @@ function tweet() {
     "statuses/update",
     {
       status: `
-      Jesus Loves You! ❤️ 
-      \n ${time.toDateString()}
+      Jesus Loves You! ❤️\n${time.toDateString()}
     `
     },
-    (err, data, response) => {
+    err => {
       if (err) {
         // eslint-disable-next-line no-console
         console.log("error", err);
         throw new Error(err);
       } else {
         // eslint-disable-next-line no-console
-        console.log("data => ", data);
-
-        // eslint-disable-next-line no-console
-        console.log("response => ", response);
+        console.log("Tweeted successfully!");
       }
     }
   );
-  // eslint-disable-next-line no-console
-  console.log("Hey! Tweeting!");
 }
 
 // tweet();
 
-// setInterval(tweet, 86400000);
-setInterval(tweet, 500000);
+setInterval(tweet, 86400000);
+// setInterval(tweet, 500000);
+
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" }); // http header
+
+    var url = req.url;
+
+    if (url === "/") {
+      res.write(`
+        <html>
+          <head>
+            <title>HowHeLoves Twitter Bot</title>
+          </head>
+          <body style="background-color: pink; color: white;">
+          <div>
+          <h2>HowHeLoves❤️</h2>
+          <p>Follow HowHeLoves❤️ on <a href="https://twitter.com/HowHeLoves3">Twitter</a></p>
+        </div>
+          </body>
+        </html>
+      `); //write a response
+      res.end(); //end the response
+    } else {
+      res.write(`
+      <h4 style="color: pink;">Woops! Page not found :/</h4>
+      <h1>Jesus <i>still</i> Loves You! tho :) ❤️<h1>
+      `); //write a response
+      res.end(); //end the response
+    }
+  })
+  .listen(port, function() {
+    // eslint-disable-next-line no-console
+    console.log(`Bot server started at port ${port}`);
+  });
 
 //  Now tweets every 24 hours
