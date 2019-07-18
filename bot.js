62734bfebfdc86886a833377a190c0fb8d3c4009
LeanKhan <eslean20@gmail.com>
2019-07-18 23:07:57 +0100

    fix: add simple http server && change main file name

--- bot.js
+++ bot.js
@@ -1,6 +1,29 @@
 ﻿const twit = require("twit"),
   config = require("./config.js");
 
+var http = require("http"); //create a server object
+
+var port = process.env.PORT || 3000;
+
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" }); // http header
+
+    var url = req.url;
+
+    if (url === "/") {
+      res.write("<h1>Hi!<h1>"); //write a response
+      res.end(); //end the response
+    } else {
+      res.write("<h1>Jesus Loves You! ❤️<h1>"); //write a response
+      res.end(); //end the response
+    }
+  })
+  .listen(port, function() {
+    // eslint-disable-next-line no-console
+    console.log(`Bot server started at port ${port}`);
+  });
+
 // eslint-disable-next-line no-console
 console.log("Bot is alive!");
 
