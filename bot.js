4ffc29dce8aebe8f8bead3d83e6ba2751f698e66
LeanKhan <eslean20@gmail.com>
2019-07-20 23:33:08 +0100

    feat: change to AWS Lambda function

--- bot.js
+++ bot.js
@@ -1,74 +1,59 @@
-﻿const twit = require("twit"),
-  config = require("./config.js");
+﻿"use strict";
 
-var http = require("http"); //create a server object
+require("dotenv").config();
 
-var port = process.env.PORT || 3000;
+const twit = require("twit"),
+  config = require("./twit_config");
 
 // eslint-disable-next-line no-console
 console.log("Bot is alive!");
 
 var Twitter = new twit(config);
 
-function tweet() {
+// function tweet() {
+//   let time = new Date();
+//   Twitter.post(
+//     "statuses/update",
+//     {
+//       status: `
+//       Jesus Loves You! ❤️\n${time.toDateString()}
+//     `
+//     },
+//     err => {
+//       if (err) {
+//         // eslint-disable-next-line no-console
+//         console.log("error", err);
+//         throw new Error(err);
+//       } else {
+//         // eslint-disable-next-line no-console
+//         console.log("Tweeted successfully!");
+//       }
+//     }
+//   );
+// }
+
+// ===== SERVERLESS ==== //
+
+module.exports.tweet = (event, context, callback) => {
   let time = new Date();
   Twitter.post(
     "statuses/update",
     {
       status: `
-      Jesus Loves You! ❤️\n${time.toDateString()}
+      Jesus Loves You! ❤️\n\n${time.toDateString()}
     `
     },
     err => {
       if (err) {
         // eslint-disable-next-line no-console
         console.log("error", err);
-        throw new Error(err);
+        // throw new Error(err);
+        callback(err);
       } else {
         // eslint-disable-next-line no-console
         console.log("Tweeted successfully!");
+        callback(null, "Tweet successful!");
       }
     }
   );
-}
-
-// tweet();
-
-setInterval(tweet, 86400000);
-// setInterval(tweet, 500000);
-
-http
-  .createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" }); // http header
-
-    var url = req.url;
-
-    if (url === "/") {
-      res.write(`
-        <html>
-          <head>
-            <title>HowHeLoves Twitter Bot</title>
-          </head>
-          <body style="background-color: pink; color: white;">
-          <div>
-          <h2>HowHeLoves❤️</h2>
-          <p>Follow HowHeLoves❤️ on <a href="https://twitter.com/HowHeLoves3">Twitter</a></p>
-        </div>
-          </body>
-        </html>
-      `); //write a response
-      res.end(); //end the response
-    } else {
-      res.write(`
-      <h4 style="color: pink;">Woops! Page not found :/</h4>
-      <h1>Jesus <i>still</i> Loves You! tho :) ❤️<h1>
-      `); //write a response
-      res.end(); //end the response
-    }
-  })
-  .listen(port, function() {
-    // eslint-disable-next-line no-console
-    console.log(`Bot server started at port ${port}`);
-  });
-
-//  Now tweets every 24 hours
+};
