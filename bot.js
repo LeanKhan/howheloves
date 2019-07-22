ad9d191f0b593b9bd9c95b2a7170b0b8e5114302
LeanKhan <eslean20@gmail.com>
2019-07-22 01:30:45 +0100

    feat: add moment-timezone && fix callback err

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@
 require("dotenv").config();
 
 const twit = require("twit"),
-  config = require("./twit_config");
+  config = require("./twit_config"),
+  moment = require("moment-timezone");
 
 // eslint-disable-next-line no-console
 console.log("Bot is alive!");
@@ -12,26 +13,60 @@ var Twitter = new twit(config);
 
 // ===== SERVERLESS ===== //
 
-module.exports.tweet = (event, context, callback) => {
-  let time = new Date();
+function tweet(time) {
   Twitter.post(
     "statuses/update",
     {
       status: `
-      Jesus Loves You! ❤️\n\n${time.toDateString()}
+      Jesus Loves You! ❤️\n\n${moment(time.getTime())
+        .tz("Africa/Lagos")
+        .format("dddd, MMM DD YYYY z")}
     `
     },
     err => {
       if (err) {
         // eslint-disable-next-line no-console
-        console.log("error", err);
-        // throw new Error(err);
-        callback(null, "Error occurred => " + err);
-      } else {
-        // eslint-disable-next-line no-console
-        console.log("Tweeted successfully!");
-        callback(null, "Tweet successful!");
+        // console.log("Twit Error => ", err);
+        throw new Error(err);
       }
     }
   );
+}
+
+module.exports.tweet = (event, context, callback) => {
+  let time = new Date();
+
+  try {
+    tweet(time);
+    return callback(null, "Tweeted Successfully");
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log("Twit Error => ", err);
+    return callback(null, "Success, though error");
+  }
+
+  // Twitter.post(
+  //   "statuses/update",
+  //   {
+  //     status: `
+  //     Jesus Loves You! ❤️\n\n${time.toDateString()}
+  //   `
+  //   },
+  //   err => {
+  //     if (err) {
+  //       // eslint-disable-next-line no-console
+  //       console.log("Twit Error => ", err);
+  //       // eslint-disable-next-line no-console
+  //       console.log("Time => ", time);
+  //       // throw new Error(err);
+  //       // context.success();
+  //       return callback(null);
+  //     } else {
+  //       // eslint-disable-next-line no-console
+  //       console.log("Tweeted successfully!");
+  //       // context.success();
+  //       return callback(null);
+  //     }
+  //   }
+  // );
 };
